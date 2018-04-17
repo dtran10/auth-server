aa32ac7218e34a1238177a35762006f2a404dc73
David Tran <trandavid1992@gmail.com>
2018-04-17 18:59:42 -0400

    Adding the rest of the jpa fields for Client

--- src/main/kotlin/com/auth/authserver/client/Client.kt
+++ src/main/kotlin/com/auth/authserver/client/Client.kt
@@ -6,4 +6,27 @@ import javax.persistence.Table
 
 @Entity
 @Table(name = "oauth_client_details")
-data class Client(@Id val clientId: String)
+data class Client(
+        @Id
+        val clientId: String? = null,
+
+        val resourceIds: String? = null,
+
+        val clientSecret: String? = null,
+
+        val scope: String? = null,
+
+        val authorizedGrantTypes: String? = null,
+
+        val webServerRedirectUri: String? = null,
+
+        val authorities: String? = null,
+
+        val accessTokenValidity: Int? = null,
+
+        val refreshTokenValidity: Int? = null,
+
+        val additionalInformation: String? = null,
+
+        val autoApprove: String? = null
+)
